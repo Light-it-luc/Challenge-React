965400da3f154a850fe944d2d4ecf0a1cd5a46ec
Lucas Cobham <lucas.cobham@lightit.io>
2024-02-08 14:23:54 -0300

    refactor: extract svg icons to their own components

--- 1-ECommerce/src/components/Searchbar.tsx
+++ 1-ECommerce/src/components/Searchbar.tsx
@@ -1,16 +1,21 @@
 import { useState } from "react"
 
+import { MagnifierIcon } from "~/ui/Icons"
+
 export const Searchbar = () => {
     const [searchValue, setSearchValue] = useState('')
 
     return (
-        <input
-            className="w-full rounded-xl md:w-44"
-            style={{ background: 'no-repeat center right 1rem url("/images/Magnifer.svg")' }}
-            type="text"
-            value={searchValue}
-            placeholder="Search"
-            onChange={(event) => setSearchValue(event.target.value)}
-        />
+        <div className="relative w-full rounded-xl md:w-44">
+            <input
+                className="w-full h-full rounded-xl"
+                type="text"
+                value={searchValue}
+                placeholder="Search"
+                onChange={(event) => setSearchValue(event.target.value)}
+            />
+
+            <MagnifierIcon className="absolute right-4 top-3" />
+        </div>
     )
 }
--- 1-ECommerce/src/components/SortBy.tsx
+++ 1-ECommerce/src/components/SortBy.tsx
@@ -1,16 +1,22 @@
 
+import { SearchArrowIcon } from "~/ui/Icons";
+
 interface SortByProps {
     children: React.ReactNode
 }
 
 export const SortBy = ({ children }: SortByProps) => {
     return (
-        <select
-            className="w-full rounded-xl text-gray-500 md:w-44"
-            style={{ background: 'no-repeat center right 1rem url("/images/SearchArrow.svg")' }}
-            name="sort-by"
-            id="sort-by">
-            {children}
-        </select>
+        <div className="relative w-full h-full md:w-44">
+            <select
+                className="w-full h-full rounded-xl bg-none text-gray-500"
+                style={{  }}
+                name="sort-by"
+                id="sort-by">
+                {children}
+            </select>
+
+            <SearchArrowIcon className="absolute right-4 top-3" />
+        </div>
     )
 }
