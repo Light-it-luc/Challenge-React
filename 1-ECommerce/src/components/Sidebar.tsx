a5bba44b4e517fb62a19be2887965393c6ef2e83
Lucas Cobham <lucas.cobham@lightit.io>
2024-02-09 10:06:54 -0300

    style: refactor to implicit return

--- 1-ECommerce/src/components/Sidebar.tsx
+++ 1-ECommerce/src/components/Sidebar.tsx
@@ -5,14 +5,11 @@ interface SidebarProps {
     children: React.ReactNode
 }
 
-export const Sidebar = ({ isMenuVisible, classWhenActive, children }: SidebarProps) => {
-    return (
-        <div
-            className={`fixed top-0 -left-48 flex flex-col gap-8 py-20 px-4
-            h-full w-48 border bg-white overflow-scroll transition-transform
-            ${( isMenuVisible ? classWhenActive : '' )}`}
-        >
-            {children}
-        </div>
-    )
-}
+export const Sidebar = ({ isMenuVisible, classWhenActive, children }: SidebarProps) =>
+    <div
+        className={`fixed top-0 -left-48 flex flex-col gap-8 py-20 px-4
+        h-full w-48 border bg-white overflow-scroll transition-transform
+        ${( isMenuVisible ? classWhenActive : '' )}`}
+    >
+        {children}
+    </div>
