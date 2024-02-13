b0e27446ced01ac25df6b2fbe44f40d7a33f0f2e
Lucas Cobham <lucas.cobham@lightit.io>
2024-02-13 13:33:20 -0300

    fix: center loader

--- 1-ECommerce/src/components/Loader.tsx
+++ 1-ECommerce/src/components/Loader.tsx
@@ -1,5 +1,5 @@
 export const Loader = () => (
-  <div className="relative flex items-center">
+  <div className="absolute top-1/2 flex items-center">
     <div
       className="h-40 w-40 animate-rotate-loader rounded-full
       border-[1.5rem] border-red-200 border-t-red-600"
