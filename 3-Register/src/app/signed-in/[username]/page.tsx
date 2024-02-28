acb9b3612781aeec08ea48bc13675cb14d4e5489
Lucas Cobham <lucas.cobham@lightit.io>
2024-02-28 12:18:04 -0300

    fix: signed in page text color

--- 3-Register/src/app/signed-in/[username]/page.tsx
+++ 3-Register/src/app/signed-in/[username]/page.tsx
@@ -15,12 +15,12 @@ export default function SignedIn({ params: { username } }: SignedInProps) {
       <CelebrationIcon />
       <div>
         <h2
-          className={`text-3xl text-center text-bold text-green-600 ${ubuntu.className}`}
+          className={`text-3xl text-center text-bold text-red-700 ${ubuntu.className}`}
         >
           Great!
         </h2>
         <h2
-          className={`text-3xl text-center text-bold text-green-600 max-w-64 ${ubuntu.className}`}
+          className={`text-3xl text-center text-bold text-red-700 max-w-64 ${ubuntu.className}`}
         >
           You signed in as {username}
         </h2>
